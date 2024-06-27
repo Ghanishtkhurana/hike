bbdb19f1148b49bbaa2ea5e576781f4cfd19b686
ghanishtkhurana <ghanishtkhurana9@gmail.com>
2024-06-27 11:51:12 +0530

    for live

--- frontend/pages/api/index.js
+++ frontend/pages/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:8080/api";
+// export const baseURL = "http://localhost:8080/api";
+export const baseURL = "https://hike-sdmr.onrender.com/api";
+
 
 const api = axios.create({
   withCredentials: true,
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { redirect } from "next/dist/server/api-utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return <div className="">Home</div>;
 }
+
+export const getServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: "/login",
+      permanent: false,
+    },
+  };
+};
